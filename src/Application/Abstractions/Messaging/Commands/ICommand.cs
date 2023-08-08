ba0dd9b329bd19bd426fd4f97cb943464d434059
Reiysher <reiysher25@gmail.com>
2023-08-08 23:17:34 +0300

    feat(mediatr): add command contract

---
+++ src/Application/Abstractions/Messaging/Commands/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.Abstractions.Messaging.Commands;
+
+public interface ICommand : IRequest
+{
+}
+
+public interface ICommand<TResponse> : IRequest<TResponse>
+{
+}
