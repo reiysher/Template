bf70726744fe82f5a086913298103465db7f94fa
Reiysher <reiysher25@gmail.com>
2023-08-17 21:47:58 +0300

    feat(messaging): command handler abstraction

--- src/Application/Abstractions/Messaging/Commands/ICommandHandler.cs
+++ src/Application/Abstractions/Messaging/Commands/ICommandHandler.cs
@@ -2,12 +2,12 @@
 
 namespace Application.Abstractions.Messaging.Commands;
 
-public interface ICommandHandler<TCommand> : IRequestHandler<TCommand>
+public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand>
     where TCommand : ICommand
 {
 }
 
-public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
     where TCommand : ICommand<TResponse>
 {
 }
