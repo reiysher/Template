6aa30f492a2fb6cbd2283a3171cf12a7cdeb169d
Reiysher <reiysher25@gmail.com>
2023-08-20 12:55:59 +0300

    feat(messaging): add domain event handler abstraction

---
+++ src/Application/Abstractions/Messaging/DomainEvents/IDomainEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Common;
+using MediatR;
+
+namespace Application.Abstractions.Messaging.DomainEvents;
+
+public interface IDomainEventHandler<TDomainEvent> : INotificationHandler<TDomainEvent>
+    where TDomainEvent : IDomainEvent
+{
+}
