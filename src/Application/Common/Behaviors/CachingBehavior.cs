9d1b5f52cbfdf207bfec152e69fbbde9287badfd
Reiysher <reiysher25@gmail.com>
2023-08-24 12:37:42 +0300

    feat(mediator): add caching behavior

---
+++ src/Application/Common/Behaviors/CachingBehavior.cs
@@ -0,0 +1,27 @@
+﻿using Application.Abstractions.Messaging.Queries;
+using MediatR;
+
+namespace Application.Common.Behaviors;
+
+// todo: taskomask
+internal class CachingBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ICacheableQuery<TResponse>
+{
+    public async Task<TResponse> Handle(TRequest query,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        // todo: check global cache settings additionaly
+        if (!query.EnableCache)
+            return await next();
+
+        // todo: get cache key and try get value then return value
+        // todo: if value is not cached, run query, get value
+        var response = await next();
+
+        // todo: and set cache
+
+        return response;
+    }
+}
