53a677af5d9c28d367455b924ef1d286f39f9644
Reiysher <reiysher25@gmail.com>
2023-08-25 18:59:24 +0300

    feat(mediator): add behaviors

---
+++ src/Application/Common/Behaviors/Configure.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Common.Behaviors;
+
+internal static class Configure
+{
+    public static IServiceCollection AddBehaviors(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LogingBehavior<,>));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}
