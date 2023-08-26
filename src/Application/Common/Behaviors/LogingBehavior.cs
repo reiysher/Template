2fb1cf86a1998020cfdfe2011b4226d7a2ba306b
Reiysher <reiysher25@gmail.com>
2023-08-26 17:50:12 +0300

    feat(mediator): add logging behaviors

---
+++ src/Application/Common/Behaviors/LogingBehavior.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Common.Behaviors;
+
+internal class LogingBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IBaseRequest
+{
+    private readonly ILogger<TRequest> _logger;
+
+    public LogingBehavior(ILogger<TRequest> logger) =>
+        _logger = logger;
+
+    public async Task<TResponse> Handle(TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        // todo: move into config "enable sensetive data logging" and logging only if enabled.
+        _logger.LogInformation("Handling [{@Request}]", typeof(TRequest).Name);
+
+        return await next();
+    }
+}
