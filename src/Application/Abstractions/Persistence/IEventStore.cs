6a53b3aba508727f6bcf7da90ab9a51f2dae32a9
Reiysher <reiysher25@gmail.com>
2023-09-05 19:20:59 +0300

    feat(event_sourcing): add event store abstraction

---
+++ src/Application/Abstractions/Persistence/IEventStore.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Abstractions.Persistence;
+
+public interface IEventStore
+{
+    Task AppendEvent<TStream>(Guid streamId, object @event, long? expectedVersion = null)
+        where TStream : notnull;
+
+    Task<T> AggregateStream<T>(Guid streamId, long? atStreamVersion = null, DateTimeOffset? atTimestamp = null)
+        where T : notnull;
+
+    Task<List<object>> GetEvents(Guid streamId, long? atStreamVersion = null, DateTimeOffset? atTimestamp = null);
+
+    //Task<IEnumerable<EventStream>> GetAllStreams();
+
+    //EventStream CreateStream(Guid streamId, string streamName);
+}
