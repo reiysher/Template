6cdf948561b918339561f64dd23b1ed05eeb07cc
Reiysher <reiysher25@gmail.com>
2023-11-26 00:34:04 +0300

    refactor(notes): refactor notes endpoints

--- src/Host/Endpoints/NoteEndpoints.cs
+++ src/Host/Endpoints/NoteEndpoints.cs
@@ -12,32 +12,36 @@ internal static class NoteEndpoints
     {
         group.WithTags("Notes");
 
-        group.MapGet("{noteId:guid}", async (
-            [FromRoute] Guid noteId,
-            [FromServices] ISender sender,
-            CancellationToken cancellationToken) =>
-            {
-                var query = new GetNoteByIdQuery(noteId);
-                var noteDto = await sender.Send(query, cancellationToken);
-                return Results.Ok(noteDto);
-            })
+        group.MapGet("{noteId:guid}", Get)
             .Produces<NoteDto?>(StatusCodes.Status200OK, MediaTypeNames.Application.Json)
             .AllowAnonymous();
 
-        group.MapPost("", async (
-            [FromBody] CreateNoteRequest request,
-            [FromServices] ISender sender,
-            CancellationToken cancellationToken) =>
-            {
-                var command = new CreateNoteCommand(request.AuthorId, request.Title, request.Content);
-                await sender.Send(command, cancellationToken);
-
-                // todo: return Created
-                return Results.Ok();
-            })
+        group.MapPost("", Create)
             .Accepts<CreateNoteRequest>(MediaTypeNames.Application.Json)
             .AllowAnonymous();
 
         return group;
+    }
+
+    public static async Task<IResult> Get(
+            [FromRoute] Guid noteId,
+            [FromServices] ISender sender,
+            CancellationToken cancellationToken)
+    {
+        var query = new GetNoteByIdQuery(noteId);
+        var noteDto = await sender.Send(query, cancellationToken);
+        return Results.Ok(noteDto);
+    }
+
+    public static async Task<IResult> Create(
+            [FromBody] CreateNoteRequest request,
+            [FromServices] ISender sender,
+            CancellationToken cancellationToken)
+    {
+        var command = new CreateNoteCommand(request.AuthorId, request.Title, request.Content);
+        await sender.Send(command, cancellationToken);
+
+        // todo: return Created
+        return Results.Ok();
     }
 }
